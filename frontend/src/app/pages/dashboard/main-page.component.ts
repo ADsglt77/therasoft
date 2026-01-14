4c673932bb876c77d0074e01cfb3653a629b7162
Adrien <adrien.segalat@gmail.com>
2026-01-14 09:00:23 +0100

    📄 - (page) Integrate layout into dashboard main page

--- frontend/src/app/pages/dashboard/main-page.component.ts
+++ frontend/src/app/pages/dashboard/main-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 import { MenuDashboardComponent } from '../../components/navbar/menuDashboard/menu-dashboard.component';
 
 @Component({
   selector: 'app-dashboard-main-page',
   standalone: true,
-  imports: [CommonModule, MenuDashboardComponent],
+  imports: [CommonModule, RouterOutlet, MenuDashboardComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
